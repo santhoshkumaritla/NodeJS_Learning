b03358ba0d6dc8f4031a509e6e76f1834eb6d0df
santhoshkumaritla <itlasanthoshkumar@gmail.com>
2025-06-12 15:49:33 +0530

    basics of tailwind css

---
+++ 11_airbnb_styling/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+
+const userRouter = require("./routes/userRouter")
+const hostRouter = require("./routes/hostRouter")
+const rootDir = require('./utils/pathutil')
+
+const app = express()
+
+app.use(express.static(path.join(rootDir,'public')))
+
+app.use(express.urlencoded())
+app.use(userRouter)
+app.use("/host",hostRouter)
+
+app.use((req,res,next)=>{
+  res.status(404).sendFile(path.join(rootDir,"views",'404.html'))
+})
+const PORT = 3000;
+
+app.listen(PORT,()=>{
+  console.log(`Server Running on address http://localhost:${PORT}`);
+});
